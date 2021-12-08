402073e78e877ed1f40617201a6f9bfeceefb1dd
duneSpice97 <89211252+duneSpice97@users.noreply.github.com>
2021-12-07 18:02:53 -0700

    adjusted adoption paragraph width

--- src/components/Info.js
+++ src/components/Info.js
@@ -8,7 +8,7 @@ function Info({viewAdoptableCats}) {
         Looking for a fuzzy new addition to your home? Answer a few questions to see
         if we have a cat that's right for you!
       </p>
-      <button onClick={() => viewAdoptableCats}>Match with a cat!</button>
+      <button className="surveyButton" onClick={() => viewAdoptableCats}>Match with a cat!</button>
       <h2>Company Info</h2>
     </div>
   );
